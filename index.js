4300cec2c098a84c9bc29f55ff136f3201f3e172
sergei_arkhipov_1 <byrgyin@yandex.ru>
2025-03-10 13:27:37 +0300

    fix_engines

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 mainRoutes(app);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Listening on http://localhost:${port}`);
+// });
